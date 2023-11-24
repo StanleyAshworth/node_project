fecca8cb97ba213c768e1611328d11c34d850c16
Ayoubion <ayoub924000@gmail.com>
2023-11-24 20:12:27 +0100

    suppression de mon commentaire inutile

--- backend/BIN1/server/migrate.js
+++ backend/BIN1/server/migrate.js
@@ -1,5 +1,5 @@
 const connection = require("./models/db");
 require("./models/User");
 
-connection.sync({alter: true}).then(() => console.log("Database synchronized")); //aerihgaoiegruaieurgaz
+connection.sync({alter: true}).then(() => console.log("Database synchronized")); 
 
