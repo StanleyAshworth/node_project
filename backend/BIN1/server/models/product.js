1e557eac6dd1f8b28c7d60676ac0fcc72d3cedc5
HajarHouari <hajar.houari@edu.devinci.fr>
2023-12-27 19:30:51 +0100

    product

--- backend/BIN1/server/models/product.js
+++ backend/BIN1/server/models/product.js
@@ -10,76 +10,25 @@ class Product extends Model {}
 Product.init(
   {
     // ce que produit 1 contient  
-    P1: {
-      name:  {
-        type : DataTypes.STRING,
-        allowNulll : false, // le produit a forcement un nom
-        unique : true, // un seul produit porte ce nom
-      },
-      description: {
-        type : DataTypes.TEXT,
-      },
-      price : {
-        type : DataTypes.FLOAT,
-        allowNulll : false,
-      },
-      creation : {
-        type : DataTypes.DATE,
-      }, // date de creation 
+    name:  {
+      type : DataTypes.STRING,
+      allowNulll : false, // le produit a forcement un nom
+      unique : true, // un seul produit porte ce nom
+    },
+    description: {
+      type : DataTypes.TEXT,
+    },
+    price : {
+      type : DataTypes.FLOAT,
+      allowNulll : false,
+    },
+    creation : {
+      type : DataTypes.DATE,
+    }, // date de creation 
 
-      update : {
-        type : DataTypes.DATE,
-      }, // date de maj 
-      },
-
-    P2: { //pareil pour le produit 2 etc
-      name:  {
-        type : DataTypes.STRING,
-        allowNulll : false, // le produit a forcement un nom
-        unique : true, // un seul produit porte ce nom
-      },
-      description: {
-        type : DataTypes.TEXT,
-      },
-      price : {
-        type : DataTypes.FLOAT,
-        allowNulll : false,
-      },
-      creation : {
-        type : DataTypes.DATE,
-      }, // date de creation 
-
-      update : {
-        type : DataTypes.DATE,
-      }, // date de maj 
-
-      },
-
-      P3: {
-        name:  {
-          type : DataTypes.STRING,
-          allowNulll : false, // le produit a forcement un nom
-          unique : true, // un seul produit porte ce nom
-        },
-        description: {
-          type : DataTypes.TEXT,
-        },
-        price : {
-          type : DataTypes.FLOAT,
-          allowNulll : false,
-        },
-        creation : {
-          type : DataTypes.DATE,
-        }, // date de creation 
-  
-        update : {
-          type : DataTypes.DATE,
-        }, // date de maj 
-        },
-
-  },
-  {
-    sequelize: connection,
+    update : {
+      type : DataTypes.DATE,
+    }, // date de maj 
   }
 );
 
